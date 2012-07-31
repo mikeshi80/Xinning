9ab6db2fcb49e3c44af5edc8ada50481355b5030
Mike Shi <mikeshi80@gmail.com>
2012-07-31 18:20:49 +0800

    Add the intro column

--- routes/index.js
+++ routes/index.js
@@ -5,12 +5,14 @@
 
 var mock_pics = require('../mock/pics');
 var mock_newses = require('../mock/news');
+var intro = require('../mock/intro');
 
 exports.index = function(req, res){
     res.render('index',
         {
             title: 'Xinning Aparts',
             pics: mock_pics.pics,
-            newses: mock_newses.newses
+            newses: mock_newses.newses,
+            intro: intro.intro
         });
 };
