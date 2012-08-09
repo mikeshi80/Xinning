946b3d7230ebf79231f1b2f55975e9170951aad5
Mike Shi <mikeshi80@gmail.com>
2012-08-09 14:05:24 +0800

    change and hide the password to mongdb
    now use the environment parameter to store the db password.

--- conf.js
+++ conf.js
@@ -4,7 +4,7 @@ exports.conf = {
     cookie_secret : '6adfb183a4a2c94a2f92dab5ade762a47889a5a1',
     static_dir    : '/public',
     less_dir      : '/public',
-    db_url        : 'mongodb://mikeshi:749591@alex.mongohq.com:10088/xinning',
+    db_url        : 'mongodb://mikeshi:' + process.env.DB_PASSWORD + '@alex.mongohq.com:10088/xinning',
     s3_opts       : {
         accessKeyId     : process.env.ACCESS_KEY_ID,
         secretAccessKey : process.env.SECRET_ACCESS_KEY,
