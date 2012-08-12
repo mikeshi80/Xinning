71f83fe2513324856a038a73ee25d1d51e9e7e5f
Mike Shi <mikeshi80@gmail.com>
2012-08-12 14:18:38 +0800

    Apply the async to get data
    Now use the async module to avoid deep callback stack.

--- routes/index.js
+++ routes/index.js
@@ -3,21 +3,43 @@
  * GET home page.
  */
 
+var async = require('async');
+
 var mock_pics = require('../mock/pics');
 var mock_newses = require('../mock/news');
 var db_news = require('../models/news');
 var intro = require('../mock/intro');
 
 exports.index = function(req, res){
-    db_news.listTitles(5, function(err, newses) {
-        res.render('index',
-            {
-                title: 'Xinning Aparts',
-            pics: mock_pics.pics,
-            newses: newses,
-            intro: intro.intro
-            });
+    async.parallel({
+        title: function(callback) {
+                   process.nextTick(function() {
+                       callback(null, 'Xinning Aparts');
+                   });
+               },
+        pics: function(callback) {
+                  process.nextTick(function() {
+                      callback(null, mock_pics.pics);
+                  });
+              },
+        newses: function(callback) {
+                    db_news.listTitles(5, function(err, newses) {
+                        callback(err, newses);
+                    });
+                },
+        intro: function(callback) {
+                   process.nextTick(function() {
+                       callback(null, intro.intro);
+                   });
+               }
+    }, function(err, results) {
+        if (err) {
+            res.send('Something error raised, err is ' + err);
+        } else {
+            res.render('index', results);
+        }
     });
+
 };
 
 exports.admin = require('./admin');
